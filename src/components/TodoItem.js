d50b18b4db08483a65bd03fc7dad6d54b64cd7f9
Efgon <egonzalezgar@gmail.com>
2021-08-30 20:55:21 -0400

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { TodosContext } from "../App";
+
+function TodoItem(props) {
+  const dispatch = useContext(TodosContext);
+  return (
+    <li className={props.completed ? "completed" : ""}>
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          checked={props.completed}
+          //onChange={props.toggleComplete}
+          onChange={(event) =>
+            dispatch({ type: "TOGGLE_COMPLETE", payload: props.id })
+          }
+        />
+        <label>{props.title}</label>
+        <button
+          className="destroy"
+          onClick={(event) =>
+            dispatch({ type: "DELETE_TODO", payload: props.id })
+          }
+        />
+      </div>
+    </li>
+  );
+}
+
+export default TodoItem;
---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import TodoItem from "./TodoItem";
+
+function TodoList(props) {
+  return (
+    <section className="main">
+      <ul className="todo-list">
+        {props.todos.map((todo) => (
+          <TodoItem
+            title={todo.title}
+            completed={todo.completed}
+            id={todo.id}
+            key={todo.id}
+            toggleComplete={props.toggleComplete}
+            deleteTodo={props.deleteTodo}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default TodoList;
