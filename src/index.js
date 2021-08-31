a7fa651d376c0a4fc3ce5088cd564aa28df6f97b
Efgon <egonzalezgar@gmail.com>
2021-08-30 21:17:22 -0400

    more fixes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/lite-todo-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
